9ee7338f1b1cccd05371f2209f8ffedf9b961074
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-06-18 12:49:46 +0200

    Dodanie dziedziczenia

---
+++ src/ObjectOriented/inheritance/ConsoleApp/Program.cs
@@ -0,0 +1,89 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+Compressor compressor = new Compressor();
+compressor.Name = "C-01";
+compressor.Start();
+compressor.Stop();
+
+Mixer mixer = new Mixer();
+mixer.Name = "M-03";
+mixer.Start(Direction.Right);
+mixer.Stop();
+
+
+// Klasowa bazowa
+class Machine
+{
+    public string Name { get; set; }
+
+    protected bool isRunning;
+
+    public Machine()
+    {
+        isRunning = false;
+    }
+
+    public void Start()
+    {
+        isRunning = true;
+    }
+
+    public void Stop()
+    {
+        isRunning = false;
+    }
+
+}
+
+// Rozszerzamy istniejąca klasę
+
+class Compressor : Machine // Dziedziczenie (inheritence) 
+{
+    public double Pressure { get; set; }
+
+    public Compressor()
+        : base()
+    {
+    }
+
+    public new void Start()
+    {
+        base.Start();
+
+        Pressure = 100;
+        Console.WriteLine("Running Compressor");
+        
+    }
+
+    
+}
+
+class Mixer : Machine
+{
+    public int RPM { get; set; }
+    
+    public Direction CurrentDirection {  get; set; }
+
+    public Mixer()
+        : base()
+    {
+        this.CurrentDirection = Direction.Left;
+    }
+
+    public void Start(Direction direction)
+    {
+        base.Start();
+
+        this.CurrentDirection = direction;
+
+        Console.WriteLine("Running Mixer");
+    }
+}
+
+// Typ wyliczeniowy
+enum Direction
+{
+    Right,
+    Left,
+}
